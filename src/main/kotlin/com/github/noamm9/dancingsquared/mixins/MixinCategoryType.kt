cbe658fdd3039638c4ec3cd1f270671ac448632d
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-09-14 18:08:41 +0200

    chore: less bad mixin (?)

--- src/main/kotlin/com/github/noamm9/dancingsquared/mixins/MixinCategoryType.kt
+++ src/main/kotlin/com/github/noamm9/dancingsquared/mixins/MixinCategoryType.kt
@@ -2,32 +2,34 @@ package com.github.noamm9.dancingsquared.mixins
 
 import com.github.noamm9.NoammAddons
 import com.github.noamm9.ui.clickgui.enums.CategoryType
+import com.llamalad7.mixinextras.injector.ModifyReturnValue
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.injection.At
-import org.spongepowered.asm.mixin.injection.Inject
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.MethodType
+import java.util.Arrays
 
 @Mixin(CategoryType::class)
 abstract class MixinCategoryType {
     private companion object {
         @Suppress("NOTHING_TO_INLINE")
         @JvmStatic
-        @Inject(method = ["\$values"], at = [At("RETURN")], cancellable = true)
-        private inline fun addCustomCategory(cir: CallbackInfoReturnable<Array<CategoryType>>) {
+        @ModifyReturnValue(method = ["\$values"], at = [At("RETURN")])
+        private inline fun addCustomCategory(values: Array<CategoryType>): Array<CategoryType> {
             try {
-                val values = cir.returnValue
                 val constructor = MethodHandles.lookup().findConstructor(
                     CategoryType::class.java,
                     MethodType.methodType(Void.TYPE, String::class.java, Int::class.javaPrimitiveType)
                 )
-                val category = constructor.invokeWithArguments("MACRO", values.size) as CategoryType
+                val category = constructor.invokeExact("MACRO", values.size) as CategoryType
 
-                cir.returnValue = values + category
+                val extendedValues = Arrays.copyOf(values, values.size + 1)
+                extendedValues[values.size] = category
+                return extendedValues
             } catch (e: Exception) {
                 NoammAddons.logger.error("Error while adding custom category type", e)
                 e.printStackTrace()
+                return values
             }
         }
     }
